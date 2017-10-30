72784da6ea2cdc9a0c29a8b28163043f3ae6e41c
ZeynoUstun <zeynoustun@gmail.com>
2017-10-30 12:55:16 -0400

    Update w9.js

--- w9.js
+++ w9.js
@@ -1,17 +1,26 @@
+// achieved listening ubuntu@ip-172-31-17-80
+// git clone this file to the ubuntu with ssh - git clone https://github.com/ZeynoUstun/week9.git
+// pm2 start w9
+// started app 
+
 var request = require('request');
 const { Client } = require('pg');
 
 // PARTICLE PHOTON
+    // exported PHOTON_ID and PHOTON_TOKEN in the terminal 
+    // changed the particle variable 
 var device_id = process.env.PHOTON_ID;
 var access_token = process.env.PHOTON_TOKEN;
-var particle_variable = 'fototemp';
+var particle_variable = 'fototilt';
 var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 
 // AWS RDS POSTGRESQL INSTANCE
+
+    // exported the host AWSRDS_EP and the password in the terminal AWSRDS_PW
 var db_credentials = new Object();
-db_credentials.user = 'zeynoustun';
+db_credentials.user = 'zeyno';
 db_credentials.host = process.env.AWSRDS_EP;
-db_credentials.database = 'laborData';
+db_credentials.database = 'datastructure';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
@@ -20,15 +29,17 @@ var getAndWriteData = function() {
     request(device_url, function(error, response, body) {
         // Store sensor values in variables
         var device_json_string = JSON.parse(body).result;
-        var photo = JSON.parse(device_json_string).photo;
-        var tempC = JSON.parse(device_json_string).tempC;
+        var tilt = JSON.parse(device_json_string).tilt;
+        var light = JSON.parse(device_json_string).light;
 
         // Connect to the AWS RDS Postgres database
         const client = new Client(db_credentials);
         client.connect();
 
         // Construct a SQL statement to insert sensor values into a table
-        var thisQuery = "INSERT INTO sensordata VALUES (" + photo + "," + tempC + ", DEFAULT);";
+        
+            // changed thisQuery to my own sql table 
+        var thisQuery = "INSERT INTO sensorsdata VALUES (" + tilt + "," + light + ", DEFAULT);";
         console.log(thisQuery); // for debugging
 
         // Connect to the AWS RDS Postgres database and insert a new row of sensor values
@@ -41,3 +52,10 @@ var getAndWriteData = function() {
 
 // write a new row of sensor data every five minutes
 setInterval(getAndWriteData, 300000);
+
+
+
+
+
+
+
